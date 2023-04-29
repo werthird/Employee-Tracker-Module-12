8257303a9195b5a439d5198b2e4332be129ded4a
Devin Reilly <werthird2016@gmail.com>
2023-04-29 16:20:42 -0400

    got the function to add to the database

--- index.js
+++ index.js
@@ -2,42 +2,57 @@
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 const db = require('./db/connectToDB.js');
-const questions = require('./utils/questions.js');
+const { initialQues, newEmpQues } = require('./utils/questions.js');
 const fetchDB = require('./fetchDB.js');
-// const cTable = require('console.table');
+const process = require('node:process');
 
 // ====================================================================================
 
 // Function to initialize app
-function init() {
-  inquirer
-  .prompt(questions)
-  .then((answers) => {
-    console.log(answers);
+async function init() {
+  const answers = await inquirer.prompt(initialQues);
 
-    // Define request from user
-    let query;
+  // Define request from user
+  let query;
 
-    // Write db query based off user request
-    switch (answers.request) {
-      case 'View All Employees':
-        query = 'SELECT * FROM employee';
-        break;
-      case 'View All Roles':
-        query = 'SELECT * FROM role';
-        break;
-      case 'View All Roles':
-        query = 'SELECT * FROM role';
-        break;
-      case 'View All Departments':
-        query = 'SELECT * FROM department';
-        break;
-      }; 
-      fetchDB(query).then((data) => {
-        console.log(`\n${cTable.getTable(data)}\n`);
-      });
-  });
+  // Write db query based on user request
+  switch (answers.request) {
+    case 'VIEW_EMPLOYEES':
+      query = 'SELECT * FROM employee';
+      break;
+    case 'ADD_EMPLOYEE':
+      query = await newEmpData();
+      break;
+    case 'UPDATE_EMPLOYEE_ROLE':
+      query = 'SELECT * FROM employee';
+      break;
+    case 'VIEW_ROLES':
+      query = 'SELECT * FROM role';
+      break;
+    case 'VIEW_DEPARTMENTS':
+      query = 'SELECT * FROM department';
+      break;
+    case 'ADD_DEPARTMENT':
+      query = 'SELECT * FROM employee';
+      break;
+    case 'QUIT':
+      process.exit();
+    }; 
+
+    // Send query string to fetchDB function then print to console
+    fetchDB(query).then((data) => {
+      console.log(`\n${cTable.getTable(data)}\n`);
+      init();
+    });
 };
 
 // Function call to initialize app
 init();
+
+// Asks questions about new employee
+async function newEmpData() {
+  const empAnswers = await inquirer.prompt(newEmpQues);
+  const { first_name, last_name, role_id, manager_id } = empAnswers;
+  let queryString = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('${first_name}', '${last_name}', ${role_id}, ${manager_id})`;
+  return queryString;
+};
--- utils/questions.js
+++ utils/questions.js
@@ -5,17 +5,63 @@ const initialQues = [
     message: 'What would you like to do?',
     name: 'request',
     choices: [
-      'View All Employees',
-      'Add Employee',
-      'Update Employee Role',
-      'View All Roles',
-      'View All Departments',
-      'Add Department',
-      'Quit'
+      {
+        name: 'View All Employees',
+        value: 'VIEW_EMPLOYEES'
+      },
+      {
+        name: 'Add Employee',
+        value: 'ADD_EMPLOYEE'
+      },
+      {
+        name: 'Update Employee Role',
+        value: 'UPDATE_EMPLOYEE_ROLE'
+      },
+      {
+        name: 'View All Roles',
+        value: 'VIEW_ROLES'
+      },
+      {
+        name: 'View All Departments',
+        value: 'VIEW_DEPARTMENTS'
+      },
+      {
+        name: 'Add Department',
+        value: 'ADD_DEPARTMENT'
+      },
+      {
+        name: 'Quit',
+        value: 'QUIT'
+      }
     ]
   }
 ];
-// const newEmployQues = [
+
+const newEmpQues = [
+  { // First Name
+    type: 'input',
+    name: 'first_name',
+    message: 'What is the new employee first name?'
+  },
+  { // Last Name
+    type: 'input',
+    name: 'last_name',
+    message: 'What is the new employees last name?'
+  },
+  { // Role
+    type: 'input',
+    name: 'role_id',
+    message: 'What is the new employees role?'
+  },
+  { // Manager ID
+    type: 'input',
+    name: 'manager_id',
+    message: 'What is the new employees manager id?'
+  }
+];
+
+
+// const newEmpQues = [
 //   {//First Name
 //     type: 'input',
 //     message: 'What is the new employees first name?',
@@ -38,4 +84,9 @@ const initialQues = [
 //   }
 // ];
 
-module.exports = initialQues;
+module.exports = {
+  initialQues,
+  newEmpQues
+};
+
+// module.exports = initialQues;
